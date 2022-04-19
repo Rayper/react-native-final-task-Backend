5a93c0e6483445a0c51cb151db10f86e8510cde1
Rayper <raihangunawan22@gmail.com>
2022-04-19 14:58:22 +0700

    auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,42 +1,19 @@
-import { BadRequestException, Body, Controller, NotFoundException, Post } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
+import { Body, Controller, Post } from '@nestjs/common';
 import { RegisterDto } from './models/register.dto';
-import * as bcrypt from 'bcryptjs';
 import { LoginDto } from './models/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private userService: UserService) {}
+  constructor(private authService: AuthService) {}
 
   @Post('register')
   async register(@Body() body: RegisterDto) {
-    if (body.password !== body.password_confirm) {
-      throw new BadRequestException('Password do not match!');
-    }
-
-    const hashed = await bcrypt.hash(body.password, 12);
-
-    return this.userService.create({
-      email: body.email,
-      password: hashed,
-    });
+    return await this.authService.register(body);
   }
 
   @Post('login')
   async login(@Body() body: LoginDto) {
-    // karena nested jadi pakai await
-    const user = await this.userService.login({ email: body.email });
-
-    // validasi ketika email tidak terdaftar
-    if (!user) {
-      throw new NotFoundException('Email is not registered!');
-    }
-
-    // compare password yang ada dan password yang diinput
-    if (!(await bcrypt.compare(body.password, user.password))) {
-      throw new BadRequestException('Wrong Password!');
-    }
-
-    return user;
+    return await this.authService.login(body);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { User } from 'src/user/models/user.entity';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async register(body): Promise<User> {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Password do not match!');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.userService.create({
+      email: body.email,
+      password: hashed,
+    });
+  }
+
+  async login(body): Promise<User> {
+    // karena nested jadi pakai await
+    const user = await this.userService.login({ email: body.email });
+
+    // validasi ketika email tidak terdaftar
+    if (!user) {
+      throw new NotFoundException('Email is not registered!');
+    }
+
+    // compare password yang ada dan password yang diinput
+    if (!(await bcrypt.compare(body.password, user.password))) {
+      throw new BadRequestException('Wrong Password!');
+    }
+
+    return user;
+  }
+}
