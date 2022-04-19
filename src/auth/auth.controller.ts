98d43931ba9819089ce827120b19e7c7f63045e1
Rayper <raihangunawan22@gmail.com>
2022-04-19 12:42:21 +0700

    add post register validations and hashed password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import { RegisterDto } from './models/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(private userService: UserService) {}
 
   @Post('register')
-  async register(@Body() body) {
-    return this.userService.create(body);
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Password do not match!');
+    }
+
+    const hashed = await bcrypt.hash(body.password, 12);
+
+    return this.userService.create({
+      email: body.email,
+      password: hashed,
+    });
   }
 }
