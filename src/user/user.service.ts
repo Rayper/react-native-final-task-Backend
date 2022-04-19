5e3a08a6a3dc1ca276c38bad878e132d34e76650
Rayper <raihangunawan22@gmail.com>
2022-04-19 11:32:02 +0700

    migrations Database

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './models/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+}
